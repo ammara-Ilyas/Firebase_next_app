608eecf2aa3bf93944601bd48c506ce4f1ebd988
ammara-Ilyas <ismarailyas545@gmail.com>
2024-03-15 13:25:11 +0500

    Sign In Authentication

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,16 +1,22 @@
 "use client";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 
 import { app } from "../fairbase";
 import Singup from "../../../component/Singup";
 function page() {
-  const createUser = () => {
+  const createUser = (email, password) => {
     const auth = getAuth(app);
-    createUserWithEmailAndPassword(auth, "yahoo123@gmail.com", "%ffg45")
+    createUserWithEmailAndPassword(auth, (email = email), (password = password))
       .then((userCredential) => {
         const user = userCredential.user;
-        console.log(user);
+        console.log(userCredential);
         // ...
+        console.log("sign up");
       })
       .catch((error) => {
         const errorCode = error.code;
@@ -18,11 +24,46 @@ function page() {
         console.log(error);
       });
   };
+  const singInUser = (email, password) => {
+    const auth = getAuth(app);
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        if (userCredential && userCredential.user) {
+          const user = userCredential.user;
+          console.log(userCredential);
+          console.log("signin");
+        } else {
+          console.error("User authentication failed");
+        }
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(error);
+      });
+  };
+  const singoutUser = () => {
+    const auth = getAuth(app);
+    signOut(auth)
+      .then(() => {
+        console.log("User signed out successfully");
+        // Additional logic after sign out, if needed
+      })
+      .catch((error) => {
+        console.error("Error signing out:", error);
+      });
+  };
   return (
     <>
       <div>Hello</div>
       {/* <Singup /> */}
-      <button onClick={createUser}>Create user</button>
+      <button onClick={() => createUser("ammarailyas361@gmail.com", "542hj4")}>
+        Sing up
+      </button>
+      <button onClick={() => singInUser("maha175@gmail.com", "542hj4")}>
+        Sign In
+      </button>
+      <button onClick={singoutUser}>Sign out</button>
     </>
   );
 }
